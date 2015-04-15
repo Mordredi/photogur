b04dcfe07ea6a120ef2a0fc851feb8d3c8a4bfb6
Arthur <arthur.p.wright@gmail.com>
2015-04-15 13:05:45 -0400

    Generated Images controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'images/index'
+
+  get 'images/show'
+
+  get 'images/new'
+
+  get 'images/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
